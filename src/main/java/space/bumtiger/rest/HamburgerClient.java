661c99378bde1ea856241ae6911501cec3c75fd6
osparking <jbpark03@gmail.com>
2023-06-09 20:58:42 +0900

    fetches ingredient info using its ID

--- src/main/java/space/bumtiger/rest/HamburgerClient.java
+++ src/main/java/space/bumtiger/rest/HamburgerClient.java
@@ -2,7 +2,14 @@ package space.bumtiger.rest;
 
 import org.springframework.web.client.RestTemplate;
 
+import space.bumtiger.domain.Ingredient;
+
 public class HamburgerClient {
 	private RestTemplate rest = new RestTemplate();
-	
+
+	public Ingredient getIngredientById(String ingredientId) {
+		return rest.getForObject(
+				"http://localhost:8080/data-api/ingredients/{id}",
+				Ingredient.class, ingredientId);
+	}
 }
